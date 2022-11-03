db19934a0bd5a60ac0f64bd1a7c7a40bb1246abe
tercio.oliveira <fracelino2011@gmail.com>
2022-11-03 11:59:44 -0300

    ok

--- src/Estagio02LP/ListaDeProdutos.java
+++ src/Estagio02LP/ListaDeProdutos.java
@@ -1,4 +1,12 @@
 package Estagio02LP;
-
+import java.util.ArrayList;
 public class ListaDeProdutos {
+    private ArrayList<Produto> listaDeProdutos;
+    public ListaDeProdutos(){}
+    public void cadastrarProduto(Produto p){
+        listaDeProdutos.add(p);
+    }
+    public ArrayList<Produto> getProdutos() {
+
+    }
 }
